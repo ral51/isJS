9658753583f54047aa47813e3d78adfb7bf44e1a
ral51 <oraldalay@gmail.com>
2018-06-07 15:46:29 -0400

    moving files from my old repo to this one

---
+++ src/is.js
@@ -0,0 +1,257 @@
+(function (window) {
+    "use strict";
+
+    var root = window,
+
+        // Credits to abozhilov
+        PRIMITIVES = {
+            'string': 1,
+            'number': 1,
+            'undefined': 1,
+            'boolean': 1
+        },
+
+        previousis = window.is,
+        is = root.is = {};
+
+    // current version
+    is.VERSION = '0.0.2',
+
+        is._Core = {
+            '_2String': Object.prototype.toString
+        },
+
+        is._C2Type = {
+            '[object Array]': 'array',
+            '[object Boolean]': 'boolean',
+            '[object Date]': 'date',
+            '[object Function]': 'function',
+            '[object Number]': 'number',
+            '[object Object]': 'object',
+            '[object RegExp]': 'regexp',
+            '[object String]': 'string'
+        },
+
+        is.noConflict = function () {
+            root.is = previousis;
+            return this;
+        },
+
+        // return format is string
+        is.typeOF = function (v) {
+            return v === null ? String(v) : is._C2Type[is._Core._2String.call(v)] || typeof v;
+        },
+
+        is.Primitive = function (v, message) {
+            if (typeof v in PRIMITIVES || !v) {
+                return true;
+            } else {
+                if (message) {
+                    fire(message);
+                } else {
+                    return false;
+                }
+            }
+        },
+
+        is.Boolean = function (v, message) {
+            if (is.typeOF(v) === 'boolean') {
+                return true;
+            } else {
+                if (message) {
+                    fire(message);
+                } else {
+                    return false;
+                }
+            }
+        },
+
+        is.Numeric = function (v, message) {
+            if (!isNaN(parseFloat(v)) && isFinite(v)) {
+                return true;
+            } else {
+                if (message) {
+                    fire(message);
+                } else {
+                    return false;
+                }
+            }
+        },
+
+        is.Integer = function (v, message) {
+            if (is.typeOF(v) === 'number' && v % 1 === 0) {
+                return true;
+            } else {
+                if (message) {
+                    fire(message);
+                } else {
+                    return false;
+                }
+            }
+        },
+
+        is.Float = function (v, message) {
+            if (!isNaN(parseFloat(v)) && v % 1 !== 0) {
+                return true;
+            } else {
+                if (message) {
+                    fire(message);
+                } else {
+                    return false;
+                }
+            }
+        },
+
+        is.Object = function (v, message) {
+            if (Object.isObject ? Object.isObject(v) : is.typeOF(v) === 'object') {
+                return true;
+            } else {
+                if (message) {
+                    fire(message);
+                } else {
+                    return false;
+                }
+            }
+        },
+
+        is.Array = function (v, message) {
+            if (Array.isArray ? Array.isArray(v) : is.typeOF(v) === 'array') {
+                return true;
+            } else {
+                if (message) {
+                    fire(message);
+                } else {
+                    return false;
+                }
+            }
+        },
+
+        is.Function = function (v, message) {
+            if (is.typeOF(v) === 'function') {
+                return true;
+            } else {
+                if (message) {
+                    fire(message);
+                } else {
+                    return false;
+                }
+            }
+        },
+
+        is.RegExp = function (v, message) {
+            if (is.typeOF(v) === 'regexp') {
+                return true;
+            } else {
+                if (message) {
+                    fire(message);
+                } else {
+                    return false;
+                }
+            }
+        },
+
+        // taken from effectiveJS book by David Herman
+        is.ReallyNaN = function (v, message) {
+            if (v !== v) {
+                return true;
+            } else {
+                if (message) {
+                    fire(message);
+                } else {
+                    return false;
+                }
+            }
+        },
+
+        is.Undefined = function (v, message) {
+            if (typeof v in PRIMITIVES && typeof v === 'undefined') {
+                return true;
+            } else {
+                if (message) {
+                    fire(message);
+                } else {
+                    return false;
+                }
+            }
+        },
+
+        is.String = function (v, message) {
+            if (typeof v in PRIMITIVES && typeof v === 'string') {
+                return true;
+            } else {
+                if (message) {
+                    fire(message);
+                } else {
+                    return false;
+                }
+            }
+        },
+
+        is.BlankString = function (v, message) {
+            if (typeof v in PRIMITIVES && typeof v === 'string' && v === '') {
+                return true;
+            } else {
+                if (message) {
+                    fire(message);
+                } else {
+                    return false;
+                }
+            }
+        },
+
+        is.BooleanTrue = function (v, message) {
+            if (v && (v === !!v)) {
+                return true;
+            } else {
+                if (message) {
+                    fire(message);
+                } else {
+                    return false;
+                }
+            }
+        },
+
+        is.NegZero = function (v, message) {
+            // return v === 0 && 1/v === -Infinity;
+
+            if (v !== 0) {
+                if (message) {
+                    fire(message);
+                } else {
+                    return false;
+                }
+            }
+
+            var obj = Object.freeze({ z: -0 });
+
+            try {
+                Object.defineProperty(obj, 'z', { value: v });
+            } catch (e) {
+                if (message) {
+                    fire(message);
+                } else {
+                    return false;
+                }
+            }
+
+            return true;
+        };
+
+    function fire(message) {
+        throw {
+            name: 'Error',
+            message: message
+        };
+    }
+
+    // NodeJS compatibility
+    if (typeof exports !== 'undefined') {
+        if (typeof module !== 'undefined' && module.exports) {
+            exports = module.exports = is;
+        }
+        exports.is = is;
+    } else {
+        root.is = is;
+    }
+
+})(this);
---
+++ test/istest.js
@@ -0,0 +1,215 @@
+describe("Is A test!", function () {
+    "use strict";
+
+    beforeEach(function () {
+    });
+
+    it("isNumericFalsy", function () {
+        var Falsy = [{}, undefined, '', null, []];
+
+        for (var i in Falsy) {
+            expect(is.Numeric(Falsy[i])).toBe(false);
+        }
+        expect(function () { is.Numeric(Falsy[0], "Is not Numeric"); }).toThrow("Is not Numeric");
+    });
+
+    it("isNumericTruthy", function () {
+        var Truthy = ['-1', '0', '0.5', '0.000000001', '100000000'];
+
+        for (var j in Truthy) {
+            expect(is.Numeric(Truthy[j])).toBe(true);
+        }
+    });
+
+    it("isPrimitiveFalsy", function () {
+        var Falsy = [{}, []];
+        for (var i in Falsy) {
+            expect(is.Primitive(Falsy[i])).toBe(false);
+        }
+        expect(function () { is.Primitive(Falsy[0], "Is not Primitive"); }).toThrow("Is not Primitive");
+    });
+
+    it("isPrimitiveTruthy", function () {
+        var Truthy = [undefined, false, true, 0, '1', -1, '', null];
+
+        for (var j in Truthy) {
+            expect(is.Primitive(Truthy[j])).toBe(true);
+        }
+    });
+
+    it("isIntegerTruthy", function () {
+        var ints = [0, -1, 1, 23412, -23412, -32345454545454, -0];
+
+        for (var j in ints) {
+            expect(is.Integer(ints[j])).toBe(true);
+        }
+    });
+
+    it("isFloatTruthy", function () {
+        var floats = [-1.5, 1.01, 2341.200, -3.2345454545454];
+
+        for (var j in floats) {
+            expect(is.Float(floats[j])).toBe(true);
+        }
+    });
+
+    it("isFloatFalsy", function () {
+        var floats = [true, 1, 23, 0, -0, {}, [], ''];
+
+        for (var j in floats) {
+            expect(is.Float(floats[j])).toBe(false);
+        }
+    });
+
+    it("isBooleanFalsy", function () {
+        var bools = [1, 23, 0, -0, {}, [], ''];
+
+        for (var j in bools) {
+            expect(is.Boolean(bools[j])).toBe(false);
+        }
+    });
+
+    it("isBooleanTruthy", function () {
+        var bools = [true, false];
+
+        for (var j in bools) {
+            expect(is.Boolean(bools[j])).toBe(true);
+        }
+    });
+
+    it("isFunctionFalsy", function () {
+        var fns = [1, 23, 0, -0, {}, [], '', true];
+
+        for (var j in fns) {
+            expect(is.Function(fns[j])).toBe(false);
+        }
+    });
+
+    it("isFunctionTruthy", function () {
+        var fns = [function a() { }];
+
+        for (var j in fns) {
+            expect(is.Function(fns[j])).toBe(true);
+        }
+    });
+
+    it("isArrayFalsy", function () {
+        var arrays = [-1, 0, undefined, {}, null, function a() { }, 0.1, ''];
+
+        for (var j in arrays) {
+            expect(is.Array(arrays[j])).toBe(false);
+        }
+    });
+
+    it("isArrayTruthy", function () {
+        var arrays = [[], [undefined, 1]];
+
+        for (var j in arrays) {
+            expect(is.Array(arrays[j])).toBe(true);
+        }
+    });
+
+    it("isNegZeroTruthy", function () {
+        var v = -0;
+        expect(is.NegZero(v)).toBe(true);
+    });
+
+    it("isNegZeroFalsy", function () {
+        var v = 1;
+        expect(is.NegZero(v)).toBe(false);
+    });
+
+    it("isRegExpTruthy", function () {
+        var regexps = [/^foo(bar)?$/i, new RegExp()];
+        for (var j in regexps) {
+            expect(is.RegExp(regexps[j])).toBe(true);
+        }
+    });
+
+    it("isRegExpFalsy", function () {
+        var regexps = [undefined, null, 0, 0.1, true, {}, [], '$'];
+        for (var j in regexps) {
+            expect(is.RegExp(regexps[j])).toBe(false);
+        }
+    });
+
+    it("isObjectTruthy", function () {
+        var objs = [{}];
+
+        for (var j in objs) {
+            expect(is.Object(objs[j])).toBe(true);
+        }
+    });
+
+    it("isObjectFalsy", function () {
+        var objs = [1, 23, 0, -0, [], '', true];
+
+        for (var j in objs) {
+            expect(is.Object(objs[j])).toBe(false);
+        }
+    });
+
+    it("isReallyNaNTruthy", function () {
+        expect(is.ReallyNaN(NaN)).toBe(true);
+    });
+
+    it("isReallyNaNFalsy", function () {
+        var nans = [1, 23, 0, -0, [], {}, true, ''];
+
+        for (var j in nans) {
+            expect(is.ReallyNaN(nans[j])).toBe(false);
+        }
+    });
+
+    it('isStringFalsy', function () {
+        var strings = [1, 23, 0, -0, [], {}, true];
+
+        for (var j in strings) {
+            expect(is.String(strings[j])).toBe(false);
+        }
+    });
+
+    it('isStringTruthy', function () {
+        var strings = ['', '1'];
+
+        for (var j in strings) {
+            expect(is.String(strings[j])).toBe(true);
+        }
+    });
+
+    it('isUndefinedFalsy', function () {
+        var undefineds = [1, 23, 0, -0, [], {}, true, null, ''];
+
+        for (var j in undefineds) {
+            expect(is.Undefined(undefineds[j])).toBe(false);
+        }
+    });
+
+    it('isUndefinedTruthy', function () {
+        expect(is.Undefined(undefined)).toBe(true);
+    });
+
+    it('isBlankStringFalsy', function () {
+        var strings = [1, 23, 0, -0, [], {}, true, 'a'];
+
+        for (var j in strings) {
+            expect(is.BlankString(strings[j])).toBe(false);
+        }
+    });
+
+    it('isBlankStringTruthy', function () {
+        expect(is.BlankString('')).toBe(true);
+    });
+
+    it('isTrueBooleanTruthy', function () {
+        expect(is.BooleanTrue(true)).toBe(true);
+    });
+
+    it('isBlankStringFalsy', function () {
+        var Falsy = [1, 23, 0, -0, [], {}, 'a'];
+
+        for (var j in Falsy) {
+            expect(is.BooleanTrue(Falsy[j])).toBe(false);
+        }
+    });
+});
